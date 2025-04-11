c68caa63bf5d83638f775d6bcd43c7fc4ea35193
nishi240masa <masa2401106@gmail.com>
2025-04-11 20:12:45 +0900

    update: DomainError に Is メソッドを実装し errors.Is に対応
    errors.Is に対応するため、DomainError に Is(error) bool を実装しました。
    これにより、ラップされたエラーも含めた柔軟な比較が可能になり、== ではなく errors.Is を用いたエラーハンドリングができるようにした。

--- src/errors/domain_error.go
+++ src/errors/domain_error.go
@@ -1,17 +1,18 @@
 package errors
 
-import "errors"
-
+import (
+	"errors"
+)
 
 type DomainError struct {
 	ErrType ErrorType
-	Err error
+	Err     error
 }
 
 type ErrorType int
 
 const (
-	InvalidParameter ErrorType = iota 
+	InvalidParameter ErrorType = iota
 	UnPemitedOperation
 	AlreadyExist
 	RepositoryError
@@ -35,13 +36,24 @@ func (e *DomainError) Unwrap() error {
 	return e.Err
 }
 
+func (e *DomainError) Is(target error) bool {
+	if e == nil {
+		return false
+	}
+	var targetErr *DomainError
+	if errors.As(target, &targetErr) {
+		return e.ErrType == targetErr.ErrType
+	}
+	return false
+}
+
 func (e *DomainError) GetType() ErrorType {
 	return e.ErrType
 }
 
-func NewDomainError( errType ErrorType, message string) *DomainError {
+func NewDomainError(errType ErrorType, message string) *DomainError {
 	return &DomainError{
 		ErrType: errType,
-		Err:   errors.New(message),
+		Err:     errors.New(message),
 	}
 }
--- src/infra/repository/user_impl.go
+++ src/infra/repository/user_impl.go
@@ -2,54 +2,49 @@ package infrastructure
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"gorm.io/gorm"
 	"w3st/domain/models"
 	"w3st/domain/repositories"
-	"w3st/errors"
-
-	"gorm.io/gorm"
+	myerrors "w3st/errors"
 )
 
 type UserRepositoryImpl struct {
-    db *gorm.DB
+	db *gorm.DB
 }
-
 
 func NewUserRepositoryImpl(db *gorm.DB) repositories.UserRepository {
 	return &UserRepositoryImpl{db: db}
 }
 
-
-
 // Create
-func (r *UserRepositoryImpl) Create(ctx context.Context, newUser *models.User)  *errors.DomainError { // context を引数に追加
+func (r *UserRepositoryImpl) Create(ctx context.Context, newUser *models.User) *myerrors.DomainError { // context を引数に追加
 	result := r.db.WithContext(ctx).Create(newUser)
 	if result.Error != nil {
 		// クエリの実行中に発生したエラー
-		return errors.NewDomainError(errors.QueryError, result.Error.Error())
+		return myerrors.NewDomainError(myerrors.QueryError, result.Error.Error())
 	}
 	// ユーザーの作成に成功した場合
 	return nil
 }
 
 // Find
-func (r *UserRepositoryImpl) FindByEmail (ctx context.Context,email string) (*models.User,  *errors.DomainError) {
+func (r *UserRepositoryImpl) FindByEmail(ctx context.Context, email string) (*models.User, *myerrors.DomainError) {
 	var user models.User
 	result := r.db.WithContext(ctx).Where("email = ?", email).First(&user)
 	// エラーが発生した場合
 	if result.Error != nil {
-		if result.Error == gorm.ErrRecordNotFound {
-			return nil, errors.NewDomainError(errors.QueryDataNotFoundError, "ユーザーが見つかりません")
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			fmt.Println("ユーザーが見つかりません!!:", result.Error)
+			return nil, myerrors.NewDomainError(myerrors.QueryDataNotFoundError, "ユーザーが見つかりません")
 		}
 		// その他のエラー
-		return nil, errors.NewDomainError(errors.QueryError, result.Error.Error())
+		fmt.Println("その他のエラー:", result.Error)
+		return nil, myerrors.NewDomainError(myerrors.QueryError, result.Error.Error())
+
 	}
 
-	// ユーザーが見つからなかった場合
-	if result.RowsAffected == 0 {
-		return nil, errors.NewDomainError(errors.QueryDataNotFoundError, "ユーザーが見つかりません")
-	}
-	
 	return &user, nil
-	
-}
 
+}
